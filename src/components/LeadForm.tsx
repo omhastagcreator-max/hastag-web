938a0347e86a52157fd891d2a3a8a79248588ae5
omhastagcreator-max <omhastagcreator@gmail.com>
2026-04-04 11:02:39 +0530

    Update LeadForm.tsx

--- src/components/LeadForm.tsx
+++ src/components/LeadForm.tsx
@@ -10,7 +10,7 @@ const LeadForm = () => {
     e.preventDefault();
     setIsSubmitting(true);
 
-    const whatsappNumber = "9450010826";
+    const whatsappNumber = "8059957479";
     const text = `New Audit Request!
 *Name:* ${form.name}
 *Email:* ${form.email}
